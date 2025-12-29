3a31e7c2374610693711deeef03bad349383f3eb
jerrinvarghese <jerrinvarghese2008@gmail.com>
2025-12-29 12:57:47 +0530

    Section 9.75 Add shop component and dispaly all the images without details of image

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -5,23 +5,15 @@ import { HttpClient } from '@angular/common/http';
 import { Product } from './shared/models/product';
 import { Pagination } from './shared/models/pagination';
 import { ShopService } from './core/services/shop.service';
+import { ShopComponent } from './features/shop/shop.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit{
-  private shopService=inject(ShopService);
-   title = 'client';
-   products:Product[]=[];
-
-   ngOnInit(): void {
-    this.shopService.getProducts().subscribe({
-     next: response=>this.products=response.data,
-     error: error=>console.log(error)
-   })
-  }
+export class AppComponent{
+  title='Skinet';
 }
